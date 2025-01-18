f9a7f4abf769d5397c8b18905a78e92a86f0119f
unknown <mohassinhussain22@gmail.com>
2025-01-18 13:39:52 +0530

    Final

--- Task 2. django-project/chat/chatapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from chatapp.models import Users
-from chatapp.models import Message
-# Register your models here.
-
-admin.site.register(Users)
-admin.site.register(Message)
-
